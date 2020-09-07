4eaef4613feef8fafba84e86bd42554025d6663d
Lycolia <sazanami@iona.2-d.jp>
2020-09-07 21:49:21 +0900

    autoformat

--- mylogger.py
+++ mylogger.py
@@ -1,18 +1,20 @@
+from MySQLConnector.mysqlconnector import MySqlConnector
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from MySQLConnector.mysqlconnector import MySqlConnector
+
 
 class LogLevel:
-  info = 1
-  warn = 2
-  error = 3
-  fatal = 4
-  debug = 5
+    info = 1
+    warn = 2
+    error = 3
+    fatal = 4
+    debug = 5
+
 
 class MyLogger:
 
-    def __init__(self, func = ''):
+    def __init__(self, func=''):
         self.db = MySqlConnector()
         self.func = func
 
@@ -20,7 +22,8 @@ class MyLogger:
         print(subject, detail)
         try:
             self.db.cur.execute(
-                "INSERT INTO Logs (logFrom, level, subject, detail) VALUES (%(func)s, %(level)s, %(subject)s, %(detail)s)",
+                "INSERT INTO Logs (logFrom, level, subject, detail)"
+                " VALUES (%(func)s, %(level)s, %(subject)s, %(detail)s)",
                 {
                     'func': self.func,
                     'level': logLv,
@@ -31,19 +34,19 @@ class MyLogger:
             self.db.conn.commit()
         except:
             raise
-    
+
     def info(self, subject, detail):
         self.putLog(LogLevel.info, subject, detail)
 
     def warn(self, subject, detail):
         self.putLog(LogLevel.warn, subject, detail)
-    
+
     def error(self, subject, detail):
         self.putLog(LogLevel.error, subject, detail)
-    
+
     def fatal(self, subject, detail):
         self.putLog(LogLevel.fatal, subject, detail)
-    
+
     def debug(self, subject, detail):
         self.putLog(LogLevel.debug, subject, detail)
 
